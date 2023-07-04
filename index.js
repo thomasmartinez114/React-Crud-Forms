5dd3b728eec4ef0bfe82f216b884ab6987b4529f
Thomas Martinez <thomas.martinez@igt.com>
2023-07-03 23:43:06 -0400

    connected app to local mysql and have it insert to db

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const cors = require('cors'); // used to access backend API in our React Front-end
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'Welcome@123!!!',
+  database: 'forms',
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  const sqlInsert =
+    "INSERT INTO gnie_update_request (date, article_number, jurisdiction, description) VALUES ('2023-07-03', '111', 'Rhode Island', 'No Description')";
+  db.query(sqlInsert, (err, result) => {
+    console.log('error', err);
+    console.log('result', result);
+    res.send('Hello Express');
+  });
+});
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
