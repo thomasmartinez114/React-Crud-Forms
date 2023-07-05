5a964d049d2924ed1fff7cf4a9dac04a301d9fcd
Thomas Martinez <thomas.martinez@igt.com>
2023-07-05 13:48:00 -0400

    updated api url / added .post to server

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,7 +9,9 @@ const Home = () => {
 
   const loadData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/get');
+      const response = await axios.get(
+        'http://localhost:5000/api/gnie_update_request'
+      );
       setData(response.data);
     } catch (err) {
       console.log(err);
--- server/index.js
+++ server/index.js
@@ -15,11 +15,26 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/get', (req, res) => {
+app.get('/api/gnie_update_request', (req, res) => {
   const sqlGet = 'SELECT * FROM gnie_update_request;';
   db.query(sqlGet, (error, result) => {
     res.send(result);
   });
+});
+
+app.post('/api/gnie_update_request', (req, res) => {
+  const { date, articleNumber, jurisdiction, description } = req.body;
+  const sqlInsert =
+    'INSERT INTO gnie_update_request (date, article_number, jurisdiction, description) VALUES (?, ?, ?, ?)';
+  db.query(
+    sqlInsert,
+    [date, articleNumber, jurisdiction, description],
+    (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+    }
+  );
 });
 
 app.get('/', (req, res) => {
