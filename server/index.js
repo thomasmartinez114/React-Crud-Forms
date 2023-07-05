0454bb5fa5340bbc085d89696ccad1b14210d875
Thomas Martinez <thomas.martinez@igt.com>
2023-07-04 20:10:37 -0400

    moved server files to server dir

--- server/index.js
+++ server/index.js
@@ -15,9 +15,16 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/api/get', (req, res) => {
+  const sqlGet = 'SELECT * FROM gnie_update_request;';
+  db.query(sqlGet, (error, result) => {
+    res.send(result);
+  });
+});
+
 app.get('/', (req, res) => {
   //   const sqlInsert =
-  //     "INSERT INTO gnie_update_request (date, article_number, jurisdiction, description) VALUES ('2023-07-03', '111', 'Rhode Island', 'No Description')";
+  //     "INSERT INTO gnie_update_request (date, article_number, jurisdiction, description) VALUES ('2023-07-03', '123', 'Rhode Island', 'No Description')";
   //   db.query(sqlInsert, (err, result) => {
   //     console.log('error', err);
   //     console.log('result', result);
