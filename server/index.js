f68c872b6b644f8ece6d19aff1738c20c9a947f5
Thomas Martinez <thomas.martinez@igt.com>
2023-07-05 09:43:31 -0400

    server side - installed mysql and required it instead of mysql2 and now data renders

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 const cors = require('cors'); // used to access backend API in our React Front-end
 
 const db = mysql.createPool({
